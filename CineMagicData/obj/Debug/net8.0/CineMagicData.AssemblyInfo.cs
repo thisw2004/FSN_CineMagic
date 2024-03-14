5fb977c35af556e9ce4417c22509c79dbe85fbb9
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-03-14 23:14:36 +0100

    -

--- CineMagicData/obj/Debug/net8.0/CineMagicData.AssemblyInfo.cs
+++ CineMagicData/obj/Debug/net8.0/CineMagicData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2900294abe562dedfbe49023b6458493283f4310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4ae15300771d5a09a9434d1d4754621b3383e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
