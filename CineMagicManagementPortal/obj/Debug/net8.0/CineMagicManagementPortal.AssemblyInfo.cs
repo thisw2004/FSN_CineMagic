5a718231f44a9cc736113ff2c86f56f37829917b
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-03-15 22:48:17 +0100

    added css page to mngmt portal

--- CineMagicManagementPortal/obj/Debug/net8.0/CineMagicManagementPortal.AssemblyInfo.cs
+++ CineMagicManagementPortal/obj/Debug/net8.0/CineMagicManagementPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineMagicManagementPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a65e98b5807e8f0887cdbd60c1145bf595b024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad31d4e9e38ccaaaa8693a353b416e21f9ca426")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineMagicManagementPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineMagicManagementPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
