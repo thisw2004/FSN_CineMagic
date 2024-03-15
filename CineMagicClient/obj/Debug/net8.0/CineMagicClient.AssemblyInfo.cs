726c1b345e8627ac5eb27a63daac127eb48510d0
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-03-15 13:54:01 +0100

    Eng var van management portaal is ready

--- CineMagicClient/obj/Debug/net8.0/CineMagicClient.AssemblyInfo.cs
+++ CineMagicClient/obj/Debug/net8.0/CineMagicClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineMagicClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7374abedf6ef5387fe42e7173b7eb997ec211f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb86a5be7fb40c9b43d7ece2e6fd743ad00252d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineMagicClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineMagicClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
