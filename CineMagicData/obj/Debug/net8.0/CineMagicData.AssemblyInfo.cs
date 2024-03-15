38de4abe5bb135e54eb8b9c03f5f4baf5254bb4b
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-03-16 00:34:34 +0100

    mainlayout toegevoegd aan de blazor

--- CineMagicData/obj/Debug/net8.0/CineMagicData.AssemblyInfo.cs
+++ CineMagicData/obj/Debug/net8.0/CineMagicData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a718231f44a9cc736113ff2c86f56f37829917b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79752e3b7364ffcc7972649f1587ee83db2275a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
