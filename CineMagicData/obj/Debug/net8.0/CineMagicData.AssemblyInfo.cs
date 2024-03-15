33249ab212cfee5c292c52e6df6ea9e26b1e5182
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-03-15 22:49:45 +0100

    Changed some component names bc they may not start with lowercase character

--- CineMagicData/obj/Debug/net8.0/CineMagicData.AssemblyInfo.cs
+++ CineMagicData/obj/Debug/net8.0/CineMagicData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad31d4e9e38ccaaaa8693a353b416e21f9ca426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a718231f44a9cc736113ff2c86f56f37829917b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineMagicData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
