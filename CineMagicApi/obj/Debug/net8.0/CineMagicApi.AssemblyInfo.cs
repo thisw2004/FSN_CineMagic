f04f324d7c5af2a416ae756d5832a7cbc6cf5792
Thisw <90311899+thisw2004@users.noreply.github.com>
2024-03-21 15:26:28 +0100

    some design

--- CineMagicApi/obj/Debug/net8.0/CineMagicApi.AssemblyInfo.cs
+++ CineMagicApi/obj/Debug/net8.0/CineMagicApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineMagicApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054d9dd9ab918ad5dc9d1ceb27719f458b1854cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81677fe62fd73d9247b367b3d9192240ae904775")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineMagicApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineMagicApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
